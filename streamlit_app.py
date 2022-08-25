fbdc2f3eb1504143bed548d13874cc58bb8a1dfc
Ryan Joseph resurreccion <112044111+rjr044@users.noreply.github.com>
2022-08-25 17:42:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
-def get)fruit_load_list():
+def get_fruit_load_list():
   with my_cnx_cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
